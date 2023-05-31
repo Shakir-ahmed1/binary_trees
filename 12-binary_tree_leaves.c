bf608ba89b328a54a9383dcee9a5aecefe7f8f23
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-05-31 23:34:30 +0000

    betty style

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree)
 	{
 		if ((tree->left == NULL) && (tree->right == NULL))
+		{
 			return (1);
+		}
 		else
 		{
 			a += binary_tree_leaves(tree->left);
