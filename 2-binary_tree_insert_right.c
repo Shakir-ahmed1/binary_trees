e1867bd7c35fe0a25446da25f9782dde9eaa74ea
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-05-31 21:27:32 +0000

    bugfix: returning null for malloc failure

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	new = binary_tree_node(NULL, value);
+	if (new == NULL)
+		return (NULL);
 	if (parent->right != NULL)
 	{
 		temp = parent->right;
