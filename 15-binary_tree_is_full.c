9d47ca8ac2596d8b887db7cf58ffd9f401d9262b
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-01 17:58:18 +0000

    returning 0 if null

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,17 +4,23 @@
  * @tree: pointer to the root node of the tree to travers
  * @func: is a pointer to a function to call for each node
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int bt_is_full(const binary_tree_t *tree)
 {
 	int a, b;
 	if (tree)
 	{
-		a = binary_tree_is_full(tree->left);
-		b = binary_tree_is_full(tree->right);
-		if (((tree->right != NULL) ^ (tree->left != NULL)) || (a == 0 || b == 0))
+		a = bt_is_full(tree->left);
+		b = bt_is_full(tree->right);
+		if (((tree->right != NULL) ^ (tree->left != NULL)) || (!a || !b))
 			return (0);
 		else
 			return (1);
 	}
 	return (1);
+}
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (bt_is_full(tree));
 }
