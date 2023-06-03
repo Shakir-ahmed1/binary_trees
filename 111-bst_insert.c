04c931ac9ebd7a3876753112c74b31a32a967696
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-03 02:21:49 +0000

    submit code

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -3,7 +3,7 @@
  * bst_insert - insert a new node to the bst
  * @tree: the starting node of the bst
  * @value: the value to be inserted
- *
+ * Return: the pointer to the new node
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
@@ -15,44 +15,24 @@ bst_t *bst_insert(bst_t **tree, int value)
 		while (temp != NULL)
 		{
 			if (temp->n == value)
-			{
-				return (temp);
-			}
-			else if ((!temp->right || (temp->right && temp->right->n > value)) && temp->n < value)
+			{ return (temp); }
+			else if (!temp->right && temp->n < value)
 			{
 				new = (bst_t *) binary_tree_node(NULL, value);
 				new->parent = temp;
-				new->right = temp->right;
 				temp->right = new;
-				new->left = NULL;
-				if (new->right)
-					new->right->parent = new;
-				return (new);
-			}
-
-			else if ((!temp->left || (temp->left && temp->left->n < value)) && temp->n > value)
+				return (new); }
+			else if (!temp->left && temp->n > value)
 			{
 				new = (bst_t *) binary_tree_node(NULL, value);
 				new->parent = temp;
-				new->left = temp->left;
 				temp->left = new;
-				new->right = NULL;
-				if (new->left)
-					new->left->parent = new;
-				return (new);
-			}
+				return (new); }
 			else if (temp->n < value)
-			{
-				temp = temp->right;
-			}
+			{ temp = temp->right; }
 			else if (temp->n > value)
-			{
-				temp = temp->left;
-			}
-		}
-	}
+			{ temp = temp->left; } } }
 	new = (bst_t *) binary_tree_node(NULL, value);
 	*tree = new;
 	return (new);
-
 }
