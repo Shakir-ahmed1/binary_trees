d2d60789e35cacecb3b5723dcc3b49e080d1afce
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-04 21:35:26 +0300

    submit advanced

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -5,24 +5,42 @@
  * @value: the value to be deleted
  * Return: the pointer to the new node after removing
  */
+
 bst_t *bst_remove(bst_t *root, int value)
 {
 	bst_t *temp;
 
-	if (root)
+	if (!root)
+		return (NULL);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
 	{
-		temp = root;
-		while (temp != NULL)
+		if (!root->left)
 		{
-			if (temp->n == value)
-			{
-				return (root);
-			}
-			else if (!temp->left && !temp->right)
-			{ return (root); }
-			else if (temp->n < value)
-			{ temp = temp->right; }
-			else if (temp->n > value)
-			{ temp = temp->left; } } }
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if (!root->right)
+		{
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
+
+		temp = root->right;
+		while (temp && temp->left)
+			temp = temp->left;
+
+		root->n = temp->n;
+		root->right = bst_remove(root->right, temp->n);
+	}
+
 	return (root);
 }
+
+
